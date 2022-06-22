3435242fe5dddfe7a53455d0186526836f9122a1
Serejkaand1 <serejkaand1@yandex.ru>
2022-06-22 21:45:45 +0300

    second day learning js

--- 2 chapter.js
+++ 2 chapter.js
@@ -3,7 +3,7 @@
 
 // 2.1
 
-// alert("Я JavaScript!")
+alert("Я JavaScript!")
 
 
 
@@ -12,24 +12,24 @@
 
 // // 2.4
 
-// let name;
-// let admin;
+let name;
+let admin;
 
-// name = "Джон";
-// admin = name;
-
-
-// alert(admin);
+name = "Джон";
+admin = name;
 
 
-
-// let newPlanet = "Планета";
-// let userName = "Сергей";
+alert(admin);
 
 
 
-// // Переменная birthday можем писать в верхнем регистре, потому что дата рождения не изменна
-// // А переменная age уже вычисляется и не такая постоянная как дата рождения 
+let newPlanet = "Планета";
+let userName = "Сергей";
+
+
+
+// Переменная birthday можем писать в верхнем регистре, потому что дата рождения не изменна
+// А переменная age уже вычисляется и не такая постоянная как дата рождения 
 
 
 
@@ -41,24 +41,24 @@
 // //2.5
 
 
-// name = "Ilya";
+name = "Ilya";
 
-// alert( `hello ${1}` ); // hello 1
+alert( `hello ${1}` ); // hello 1
 
-// alert( `hello ${"name"}` ); // hello name
+alert( `hello ${"name"}` ); // hello name
 
-// alert( `hello ${name}` ); // hello Ilya
-
-
+alert( `hello ${name}` ); // hello Ilya
 
 
 
 
-// //2.6
 
-// let user = prompt("Как твое имя?", "")
 
-// alert(`Привет, ${user}`)
+//2.6
+
+let user = prompt("Как твое имя?", "")
+
+alert(`Привет, ${user}`)
 
 
 
@@ -66,45 +66,45 @@
 //2.8
 
 
-// let a = 1, b = 1;
+let a = 1, b = 1;
 
-// let c = ++a; //2
-// let d = b++; //1
-
-
+let c = ++a; //2
+let d = b++; //1
 
 
 
-// a = 2;
-
-// let x = 1 + (a *= 2); // a = 4, x = 5
 
 
-// "" + 1 + 0  // 10
-// "" - 1 + 0  //  -1
-// true + false  // 1
-// 6 / "3"   // 2
-// "2" * "3"   // 6 
-// 4 + 5 + "px"   // 9px
-// "$" + 4 + 5   // $45
-// "4" - 2   // 2
-// "4px" - 2   // NAN
-// "  -9  " + 5   // -9 5
-// "  -9  " - 5   // -14
-// null + 1   // 1
-// undefined + 1   // NAN
-// " \t \n" - 2   // -2
+a = 2;
+
+let x = 1 + (a *= 2); // a = 4, x = 5
+
+
+"" + 1 + 0  // 10
+"" - 1 + 0  //  -1
+true + false  // 1
+6 / "3"   // 2
+"2" * "3"   // 6 
+4 + 5 + "px"   // 9px
+"$" + 4 + 5   // $45
+"4" - 2   // 2
+"4px" - 2   // NAN
+"  -9  " + 5   // -9 5
+"  -9  " - 5   // -14
+null + 1   // 1
+undefined + 1   // NAN
+" \t \n" - 2   // -2
 
 
 
-// a = prompt("Первое число?", 1);
-// b = prompt("Второе число?", 2);
+a = prompt("Первое число?", 1);
+b = prompt("Второе число?", 2);
 
-// alert(a + b); // 12
+alert(a + b); // 12
 
 // Исправленный вариант
 
-// alert(+a + +b);
+alert(+a + +b);
 
 
 
@@ -138,29 +138,29 @@ null === +"\n0\n"  //false
 
 
 
-// let question = prompt("Какое «официальное» название JavaScript?", "")
-// if (question == "ECMAScript"){
-//     alert("Верно!");
-// } else{
-//     alert("Не знаете? ECMAScript!");
-// }
+let question = prompt("Какое «официальное» название JavaScript?", "")
+if (question == "ECMAScript"){
+    alert("Верно!");
+} else{
+    alert("Не знаете? ECMAScript!");
+}
 
 
 
 
-// let numb = prompt ("Введите число", "")
+let numb = prompt ("Введите число", "")
 
-// if (numb > 0){
-//     alert(1);
-// }
+if (numb > 0){
+    alert(1);
+}
 
-// else if(numb < 0){
-//     alert(-1);
-// }
+else if(numb < 0){
+    alert(-1);
+}
 
-// else{
-//     alert(0);
-// }
+else{
+    alert(0);
+}
 
 
 
@@ -173,46 +173,19 @@ null === +"\n0\n"  //false
 
 
 
-// let message = 
-// (login == 'Сотрудник') ? 'Привет' : 
-// (login == 'Директор') ? 'Здравствуйте' : 
-// (login == '') ? 'Нет логина' : 
-// '';
+let message = 
+(login == 'Сотрудник') ? 'Привет' : 
+(login == 'Директор') ? 'Здравствуйте' : 
+(login == '') ? 'Нет логина' : 
+'';
 
 
 
 
 // //2.11
 
-// alert( null || 2 || undefined ); // выведет первое true значение, то есть 2
 
-
-// alert( alert(1) || 2 || alert(3) ); // сначала выполнится первое значение, затем второе true значение, то есть сначала 1, затем 2
-
-
-
-// alert( 1 && null && 2 ); // выведет null, так как && ищет false значение
-
-// alert( alert(1) && alert(2) ); //так как alert не возвращает значения, в модальное окно выведется сначала 1, затем undefined
-
-
-// alert( null || 2 && 3 || 4 ); // вызовется 3
-
-
-// if (age >= 14 && age <= 90); //применяем оператор && потому что, если даже одно значение не подходит, решение нас не устроит)
-
-
-
-
-// if (!(age >= 14 && age <= 90))// точно такое же выражение просто с оператором !
-
-// if (age < 14 || age > 90) // здесь уже нас устраивает, если одно из значений будет true или false
-
-
-
-
-
-let firstName = prompt("Кто там?", '');
+let firstName = prompt ("Кто там?", '');
 
 if (firstName == 'Админ') {
 
@@ -231,6 +204,37 @@ if (firstName == 'Админ') {
 } else {
   alert( "Я вас не знаю" );
 }
+
+
+
+
+
+//2.13
+
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+      alert( i );
+    }
+  }
+
+
+
+
+
+    let i = 0;
+    while (i < 3) {
+    alert( `number ${i}!` );
+     i++;
+}
+
+
+
+let numb2;
+
+do {
+  num2 = prompt("Введите число больше 100", 0);
+} while (num2 <= 100 && num2);
+
 
 
 
