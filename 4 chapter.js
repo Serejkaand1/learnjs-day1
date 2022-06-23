d61ed57eea5c582dc7579aa2953a7a3708af545a
Serejkaand1 <serejkaand1@yandex.ru>
2022-06-23 18:23:02 +0300

    three day learning js

--- 4 chapter.js
+++ 4 chapter.js
@@ -4,43 +4,43 @@
 // 4.1
 
 
-// let user = {};
+let user = {};
 
-// user.name = "John";
+user.name = "John";
 
-// user.surname = "Smith";
+user.surname = "Smith";
 
-// user.name = "Pete";
+user.name = "Pete";
 
-// delete user.name;
-
-
-
-// function isEmpty(obj){
-//     for(ket in obj){
-//         return false;
-//     }
-//     return true;
-// }
+delete user.name;
 
 
 
+function isEmpty(obj){
+    for(ket in obj){
+        return false;
+    }
+    return true;
+}
 
-// let salaries = {
-//     John: 100,
-//     Ann: 160,
-//     Pete: 130
-//   };
 
 
 
-// let sum = 0;
-//        for (let key in salaries) {
-//                    sum += salaries[key];
-// }
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+  };
+
+
+
+let sum = 0;
+       for (let key in salaries) {
+                   sum += salaries[key];
+}
         
 
-// alert(sum);
+alert(sum);
 
 
 function multiplyNumeric(obj){
@@ -62,3 +62,96 @@ let menu = {
 
 
   multiplyNumeric(menu)
+
+
+
+
+
+
+
+
+
+
+// 4.4 
+
+
+
+
+let calculator = {
+    sum() {
+      return +this.a + +this.b;
+    },
+  
+    mul() {
+      return this.a * this.b;
+    },
+  
+    read() {
+      this.a = prompt('a?', 0);
+      this.b = prompt('b?', 0);
+    }
+  };
+  
+  calculator.read();
+  alert( calculator.sum() );
+  alert( calculator.mul() );
+
+
+
+
+
+
+// 4.5
+
+
+
+
+
+function Calculator() {
+
+    this.read = function() {
+      this.a = prompt('a?', 0);
+      this.b = prompt('b?', 0);
+    };
+  
+    this.sum = function() {
+      return + this.a + + this.b;
+    };
+  
+    this.mul = function() {
+      return this.a * this.b;
+    };
+  }
+
+
+
+
+
+
+
+  function Accumulator(startingValue) {
+    this.value = startingValue;
+  
+    this.read = function() {
+      this.value += +prompt('Введите число', 0);
+    };
+  
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
