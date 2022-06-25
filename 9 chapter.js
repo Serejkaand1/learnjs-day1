846e711c8c391f5108e6196e21d560b3a8ecd30d
Serejkaand1 <serejkaand1@yandex.ru>
2022-06-25 23:06:23 +0300

    five day learning js

--- 9 chapter.js
+++ 9 chapter.js
@@ -41,3 +41,20 @@ class Clock {
   
   let clock = new Clock({template: 'h:m:s'});
   clock.start();
+
+
+
+//   9.2
+
+class ExtendedClock extends Clock {
+    constructor(options) {
+      super(options);
+      let { precision=1000 } = options;
+      this.precision = precision;
+    }
+  
+    start() {
+      this.render();
+      this.timer = setInterval(() => this.render(), this.precision);
+    }
+  };
