190c11197371eaa2830905325eda376e944af190
Serejkaand1 <serejkaand1@yandex.ru>
2022-06-25 09:50:56 +0300

    five day learning js

--- 5 chapter.js
+++ 5 chapter.js
@@ -391,3 +391,26 @@ let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
 
 
 
+// 5.8
+
+
+let messages = [
+  {text: "Hello", from: "John"},
+  {text: "How goes?", from: "John"},
+  {text: "See you soon", from: "Alice"}
+];
+
+let read = new WeakSet();
+
+read.add(messages[0]);
+read.add(messages[1]);
+
+
+
+read.add(messages[0]);
+
+
+
+alert("Read message 0: " + read.has(messages[0]));
+
+messages.shift();
